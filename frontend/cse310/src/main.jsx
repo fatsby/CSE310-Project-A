8bc1be7f27cd036236072d173b63943083393a41
Le Minh Tri <jackcheatpigg@gmail.com>
2025-07-09 18:32:19 +0700

    added router

--- frontend/cse310/src/main.jsx
+++ frontend/cse310/src/main.jsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import ItemPage from './pages/ItemPage.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />, //CHANGE THIS TO HOME PAGE
+  },
+  {
+    path: '/item',
+    element: <ItemPage />,
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ frontend/cse310/src/pages/ItemPage.jsx
@@ -0,0 +1,10 @@
+function ItemPage(){
+    return (
+        <div>
+        <h1>Item Page</h1>
+        <p>Test</p>
+        </div>
+    );
+}
+
+export default ItemPage;
