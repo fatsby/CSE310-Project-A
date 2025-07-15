4d0a82b7fbd9d68dd84643b70a84b8e9fc4b07f1
fatsby <jackcheatpigg@gmail.com>
2025-07-15 13:16:12 +0700

    Fixed ItemCard to handle free items

--- frontend/cse310/src/components/ItemCard.jsx
+++ frontend/cse310/src/components/ItemCard.jsx
@@ -12,8 +12,9 @@ function ItemCard({ itemData }) {
             <img src={itemData.images[0]} alt={itemData.name} className="w-full h-48 object-cover rounded-md mb-4" />
             <p className="text-lg font-semibold text-gray-800 truncate" title={itemData.name}>{itemData.name}</p>
             <div className="flex items-center justify-between mt-2">
-                <p className="text-blue-600 font-bold">{itemData.price} VND</p>
-                <div className="flex items-center gap-x-1">
+                <p className="text-blue-600 font-bold">
+                    {itemData.price === 0 ? 'FREE' : `${itemData.price} VND`}
+                </p>                <div className="flex items-center gap-x-1">
                     <p className="font-bold text-gray-700">{itemData.avgRating}</p>
                     <Star fill="#FFCC00" color="#FFCC00" size={20} />
                 </div>
