50c744e908f62e523c135928db11e510b7ce6e84
tuttotutti <ltminhtuoi208@gmail.com>
2025-08-16 12:34:54 +0700

    updated Upload page
    -uploaded files are displayed more clearly

--- frontend/cse310/src/pages/Upload.jsx
+++ frontend/cse310/src/pages/Upload.jsx
@@ -1,8 +1,11 @@
 import { useCallback, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDropzone } from "react-dropzone";
+
 import { CheckIcon, ScrollArea } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
+
+import { X } from "lucide-react";
 
 // ===========================
 // Assets
@@ -75,18 +78,18 @@ export default function Upload({ onCloseUpload }) {
   // Component Rendering
   // ===========================
   return (
-    <div className="relative w-full h-full bg-[#f9f9f9] mx-auto aspect-[12/7]">
+    <div className="relative w-full h-fit bg-[#f9f9f9] mx-auto">
       {/* === Dropzone Area === */}
-      <div className="flex justify-center items-center h-full">
+      <div className="flex justify-center items-center">
         <div
           {...getRootProps()}
-          className="w-full h-full flex justify-center items-center rounded-[15px] "
+          className="w-full flex justify-center items-center rounded-[15px] "
         >
           <input {...getInputProps()} />
           {isDragActive ? (
-            <div className="w-full h-full flex justify-center items-center rounded-[15px] p-10">
+            <div className="w-full flex justify-center items-center rounded-[15px] p-10">
               <div
-                className="relative w-full h-full flex justify-center items-center text-center bg-[#f2f7fe] rounded-[15px]"
+                className="relative w-full flex justify-center items-center text-center bg-[#f2f7fe] rounded-[15px] aspect-[12/7]"
                 style={{ border: "2px dashed #b1d2ff" }}
               >
                 <div className="flex flex-col items-center">
@@ -104,8 +107,11 @@ export default function Upload({ onCloseUpload }) {
               </div>
             </div>
           ) : (
-            <div className="w-full h-full flex justify-center items-center rounded-[15px]">
-              <div className="relative w-full flex justify-center items-center text-center rounded-[15px] border-2 border-transparent">
+            <div className="w-full flex justify-center items-center rounded-[15px] p-10">
+              <div
+                className="relative w-full flex justify-center items-center text-center rounded-[15px] aspect-[12/7]"
+                style={{ border: "2px dashed #b1d2ff" }}
+              >
                 <div className="flex flex-col items-center">
                   <img src={BLUE_UP_IMG} alt="Upload" className="w-[80px]" />
                   <p className="text-[15px] text-[#4e5966] font-bold">
@@ -129,41 +135,47 @@ export default function Upload({ onCloseUpload }) {
 
       {/* === Uploaded File List === */}
       {files.length > 0 && (
-        <div className="mx-auto flex flex-col justify-center items-center pt-5 pb-8">
-          <div>
-            <ul className="">
+        <div className="flex flex-col items-center px-10 pb-8 bg-fff">
+          <div className="w-full">
+            <span className="px-5">
+              <span className="text-[#2596be]">{files.length}</span> Files
+              Uploaded
+            </span>
+            <ul className="mt-4">
               {files.map((file) => (
                 <li key={file.name} className="mb-4">
-                  <div className="flex items-center w-[500px] h-[60px] px-4 bg-white rounded-[10px] drop-shadow-[0_4px_12px_rgba(22,34,55,0.06)]">
-                    <div className="flex-shrink-0 w-8 h-8 mr-4">
+                  <div className="flex items-center w-full h-[60px] px-3 rounded-[10px] drop-shadow-[0_4px_12px_rgba(22,34,55,0.06)] bg-white">
+                    <div className="w-1/14 h-8 mr-4 ">
                       <img
                         src={GGDOCS_IMG}
                         alt="Document"
                         className="w-full h-full object-contain"
                       />
                     </div>
-                    <div className="flex-1 overflow-hidden">
-                      <p className="text-sm font-medium truncate">
-                        {file.name}
-                      </p>
-                      <p className="text-xs text-gray-500">
-                        {formatBytes(file.size)}
-                      </p>
+                    <div className="w-12/14 flex flex-col">
+                      <div className="flex-1 overflow-hidden items-end">
+                        <p className="text-sm font-medium truncate">
+                          {file.name}
+                        </p>
+                      </div>
+                      <div className="items-center">
+                        <span className="text-xs text-gray-500 w-[100px]">
+                          {formatBytes(file.size)}
+                        </span>
+                      </div>
                     </div>
-                    <button
-                      onClick={() => handleRemoveFile(file.name)}
-                      className="ml-4 text-[#C3C5C9] hover:text-red-600"
-                    >
-                      <i className="fa-solid fa-trash text-lg"></i>
-                    </button>
+                    <div className="w-1/14 text-center">
+                      <button
+                        onClick={() => handleRemoveFile(file.name)}
+                        className="ml-4 text-white bg-gray-300 rounded-full p-[3px] hover:bg-gray-500"
+                      >
+                        <X size={10} />
+                      </button>
+                    </div>
                   </div>
                 </li>
               ))}
             </ul>
-            <span>
-              <span className="text-[#2596be]">{files.length}</span> Files
-              Uploaded.
-            </span>
           </div>
 
           {/* === Upload Button === */}
